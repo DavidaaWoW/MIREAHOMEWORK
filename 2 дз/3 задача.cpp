5f666643924c2785c31adbe5ec2cfa1c19e3fa55
DavidaaWoW <76223295+DavidaaWoW@users.noreply.github.com>
2020-12-23 16:17:44 +0400

    Create 3 задача.cpp

---
+++ 2 дз/3 задача.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <math.h>
+#include <cstdlib>
+#include <windows.h>
+
+using namespace std;
+
+int main()
+{
+    SetConsoleCP(1251); //локализируем консоль
+SetConsoleOutputCP(1251);
+//вводим переменные
+double z;
+double x;
+double y;
+double b;
+//сообщаем пользователю уравнение функции и просим ввести значения
+cout << "Базовое уравнение функции z = ln(b - y) * sqrt(b - x)" << endl;
+cout << "Введите число b" << endl;
+cin >> b;
+cout << "Введите число y" << endl;
+cin >> y;
+cout << "Введите число x" << endl;
+cin >> x;
+//согласно свойствам корня и натурального логарифма делаем проверку на правильность введённых данных, и в зависимости от результата выводим ответ, или сообщение об ошибке
+if (b - y > 0 && b - x >= 0)
+{
+    z = log(b - y) * sqrt(b - x);
+    cout << "Ответ: z = " << z << endl;
+}
+else
+{
+    cout << "Введённые данные некорректны" << endl;
+}
+    return 0;
+}
